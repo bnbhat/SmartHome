dd94428854f1e6cc7d5bff226584e0abebeef96d
AllenP3 <72021357+AllenP3@users.noreply.github.com>
2022-11-22 02:31:34 +0100

    Update 1
    Authenticated using @AllenP3 Blynk token/profile

--- humidity.ino
+++ humidity.ino
@@ -5,12 +5,55 @@
 *
 *
 */
+#define BLYNK_TEMPLATE_ID           "TMPLUBF7APzW"
+#define BLYNK_DEVICE_NAME           "Quickstart Device"
 
+#define BLYNK_PRINT Serial
 
-void setup() {
-  
+ 
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
+ 
+
+char auth[] = "G_C2xIhqfeOPOIli6NggxkuvyM3fem_x";
+
+char ssid[] = "PyTorch";
+char pass[] = "MechFhws$013";
+ 
+#define DHTPIN 0          // D3
+ 
+
+#define DHTTYPE DHT11     // DHT 11
+ 
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
+ 
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+ 
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Blynk.virtualWrite(V5, t);
+  Blynk.virtualWrite(V6, h);
 }
+ 
+void setup()
+{
+  Serial.begin(9600);
+ 
+  Blynk.begin(auth, ssid, pass);
 
+  dht.begin();
+
+  timer.setInterval(500L, sendSensor);
+}
 void loop() {
-  
+  Blynk.run();
+  timer.run();
 }
